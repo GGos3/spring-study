9677962657d31e342484f59cf7d87cda78830a08
GGos3 <janpll0421@gmail.com>
2022-12-14 03:56:18 +0900

    xml 기반 bean 조회

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -11,7 +11,7 @@ public class XmlAppContext {
     @Test
     void xmlAppContext(){
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-        MemberService memberService = ac.getBean("memberSerice", MemberService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
