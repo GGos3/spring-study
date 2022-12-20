9a94cf780bff56cf9143df2d4f628b55695afc6f
JangFeel <janpll0421@gmail.com>
2022-12-20 13:27:39 +0900

    컴포넌트 스캔 추가

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 
+    @Autowired  // 자동 DI
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
