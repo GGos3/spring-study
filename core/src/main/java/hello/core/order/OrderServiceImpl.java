bedd7c340222ea344c4589cf6b08cdfca567cce6
JangFeel <janpll0421@gmail.com>
2022-12-29 21:39:31 +0900

    lombok 적용

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,16 +3,24 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     // 수정자 주입
+    // @RequiredArgsConstructor 와 같은 역할
+/*    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }*/
+
 /*    @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -22,8 +30,8 @@ public class OrderServiceImpl implements OrderService{
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }*/
-
     // 일반 메서드 주입
+
  /*   @Autowired
     public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
@@ -33,16 +41,10 @@ public class OrderServiceImpl implements OrderService{
     /*  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
             private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
             private final MemberRepository memberRepository = new MemoryMemberRepository();*/
-
     /*
     * 생성자 주입
     * 생성자가 1개라면 @Autowired 생략 가능
      * */
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
 
     @Override
